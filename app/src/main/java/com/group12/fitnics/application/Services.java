df2649222c68e1b4f0d1714a991f51c2083d994d
Viki4325 <Viki4325@users.noreply.github.com>
2021-02-23 16:09:29 -0600

    Updated Services, make and return exerciseStub instance

--- app/src/main/java/com/group12/fitnics/application/Services.java
+++ app/src/main/java/com/group12/fitnics/application/Services.java
@@ -1,11 +1,14 @@
 package com.group12.fitnics.application;
 
+import com.group12.fitnics.persistence.ExercisePersistence;
+import com.group12.fitnics.persistence.ExercisePersistenceStub;
 import com.group12.fitnics.persistence.UserPersistence;
 import com.group12.fitnics.persistence.UserPersistenceStub;
 
 public class Services {
 
     private static UserPersistence userPersistence = null;
+    private static ExercisePersistence exercisePersistence = null;
 
     public static UserPersistence getUserPersistence() {
         if (userPersistence == null) {
@@ -13,5 +16,13 @@ public class Services {
         }
         return userPersistence;
     }
+
+    public static ExercisePersistence getExercisePersistence() {
+        if (exercisePersistence == null) {
+            exercisePersistence = new ExercisePersistenceStub();
+        }
+        return exercisePersistence;
+    }
+
 
 }
