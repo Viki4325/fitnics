9d9dcc7fd156a59f0a34eeb332664c4af039f75e
Zijian Lai <laiz@myumanitoba.ca>
2021-02-25 22:29:43 -0600

    Update test file

--- app/src/test/java/com/group12/fitnics/tests/business/AccessFoodsTest.java
+++ app/src/test/java/com/group12/fitnics/tests/business/AccessFoodsTest.java
@@ -18,41 +18,60 @@ public class AccessFoodsTest {
     @Test
     // test search by food name
     public void TestSearchByFoodName(){
+        System.out.println("\nStarting TestSearchByFoodName");
         food = accessFood.searchByFoodName("Bread");
         assertEquals(food.getFoodID(),1);
         assertEquals(food.getCalories(), 2.33,0.01);
         assertSame(food.getName(), "Bread");
+        System.out.println("Finished TestSearchByFoodName");
+
+
     }
     @Test
         // test search by food id
     public void TestSearchByFoodID(){
+        System.out.println("\nStarting TestSearchByFoodID");
         food = accessFood.searchByFoodID(2);
         assertEquals(food.getFoodID(),2);
         assertEquals(food.getCalories(), 0.53,0.01);
         assertSame(food.getName(), "Nonfat Greek yogurt");
+        System.out.println("Finished TestSearchByFoodID");
+
+
     }
 
     @Test
     public void TestAddFood(){
+        System.out.println("\nStarting TestAddFood");
         accessFood.addFood(food);
         Food test = accessFood.searchByFoodID(10);
         assertEquals(test.getFoodID(),10);
         assertEquals(test.getCalories(), 1.5,0.01);
         assertSame(test.getName(), "Orange");
+        System.out.println("Finished TestAddFood");
+
+
     }
 
     @Test
     // test delete food
     public void TestDeleteFood(){
+        System.out.println("\nStarting TestDeleteFood");
         accessFood.addFood(food);
         assertTrue(accessFood.deleteFood(food));
         assertNull(accessFood.searchByFoodID(10));
+        System.out.println("Finished TestDeleteFood");
+
+
     }
     @Test
     public void TestUpdateFood(){
+        System.out.println("\nStarting TestUpdateFood");
         food = new Food(5, "Steamed rice", 1.93);
         food.setCalories(10);
         accessFood.updateFood(food);
         assertEquals(accessFood.searchByFoodID(5).getCalories(), 10,0.01);
+        System.out.println("Finished TestUpdateFood");
+
     }
 }
--- app/src/test/java/com/group12/fitnics/tests/persistence/FoodPersistenceStubTest.java
+++ app/src/test/java/com/group12/fitnics/tests/persistence/FoodPersistenceStubTest.java
@@ -1,64 +1,67 @@
-package com.group12.fitnics.tests.persistence;
-
-import com.group12.fitnics.objects.Food;
-import com.group12.fitnics.persistence.FoodPersistence;
-import com.group12.fitnics.persistence.FoodPersistenceStub;
+package com.group12.fitnics.tests.objects;
 
 import org.junit.Before;
 import org.junit.Test;
-import java.util.ArrayList;
+
+import com.group12.fitnics.objects.Food;
+
 import static org.junit.Assert.*;
 
-public class FoodPersistenceStubTest {
+public class FoodTest {
     private Food food;
-    FoodPersistenceStub foodPersistenceStub;
+
     @Before
     public void setUp() throws Exception {
         food = new Food(10, "Orange", 1.5);
-        foodPersistenceStub = new FoodPersistenceStub();
     }
 
     @Test
-    public void testGetFoodSequential(){
-        ArrayList<Food> list = foodPersistenceStub.getFoodSequential();
-        assertNotNull(list);
+    public void testConstructor() {
+        System.out.println("\nStarting testConstructor");
+        assertNotNull(food);
+        System.out.println("Finished testConstructor");
     }
-    @Test
-    public void testGetFoodByID(){
-        food = foodPersistenceStub.getFoodByID(1);
-        assertEquals(food.getFoodID(), 1);
-        assertSame(food.getName(), "Bread");
-        assertEquals(food.getCalories(), 2.33,0.01);
-    }
-    @Test
-    public void testGetFoodByFoodName(){
-        food = foodPersistenceStub.getFoodByFoodName("Bread");
 
-        assertEquals(food.getFoodID(), 1);
-        assertSame(food.getName(), "Bread");
-        assertEquals(food.getCalories(), 2.33,0.01);
+    @Test
+    public void testGetFoodID() {
+        System.out.println("\nStarting testGetFoodID");
+        assertEquals(10, food.getFoodID());
+        System.out.println("Finished testGetFoodID");
+    }
+
+    @Test
+    public void testGetName() {
+        System.out.println("\nStarting testGetName");
+        assertEquals("Orange", food.getName());
+        System.out.println("Finished testGetName");
     }
     @Test
-    public void testInsertFood(){
-        foodPersistenceStub.insertFood(food);
-        Food test = foodPersistenceStub.getFoodByFoodName("Orange");
-        assertEquals(7, foodPersistenceStub.getFoodSequential().size());
-        assertSame("Orange", test.getName());
-        assertEquals(10, test.getFoodID());
-        assertEquals(1.5, test.getCalories(),0.1);
+    public void testGetCalories() {
+        System.out.println("\nStarting testGetCalories");
+        assertEquals(1.5, food.getCalories(),0.1);
+        System.out.println("Finished testGetCalories");
     }
     @Test
-    public void testUpdateFood(){
-        Food food = new Food(10, "Orange", 5.5);
-        foodPersistenceStub.updateFood(6,food);
-        Food test = foodPersistenceStub.getFoodByFoodName("Orange");
-        assertEquals("Orange", test.getName());
-        assertEquals(6, test.getFoodID());
-        assertEquals(5.5, test.getCalories(),0.1);
+    public void testSetFoodID() {
+        System.out.println("\nStarting testSetFoodID");
+        food.setFoodID(123456789);
+        assertEquals(123456789, food.getFoodID());
+        System.out.println("Finished testSetFoodID");
     }
     @Test
-    public void testDeleteFood(){
-        assertTrue(foodPersistenceStub.deleteFood(6));
-        assertNull(foodPersistenceStub.getFoodByID(6));
+    public void testSetName() {
+        System.out.println("\nStarting testSetName");
+        food.setName("ABCDEFG");
+        assertEquals("ABCDEFG", food.getName());
+        System.out.println("Finished testSetName");
     }
+    @Test
+    public void testSetCalories() {
+        System.out.println("\nStarting testSetCalories");
+        food.setCalories(9999.99);
+        assertEquals(9999.99, food.getCalories(),0.01);
+        System.out.println("Finished testSetCalories");
+    }
+
+
 }
