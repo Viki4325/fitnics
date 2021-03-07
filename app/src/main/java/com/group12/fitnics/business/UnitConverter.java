cba899ff033062651c7488752891b1df7a1c5a1e
GahyunSuh <suhga415@gmail.com>
2021-03-08 01:42:28 +0900

    Fix SignUp UI and add UnitConverter in logic

---
+++ app/src/main/java/com/group12/fitnics/business/UnitConverter.java
@@ -0,0 +1,12 @@
+package com.group12.fitnics.business;
+
+public class UnitConverter {
+
+    public static double KGToLB(double kg) {
+        return kg * 2.205; // 1 kg = 2.205 lb
+    }
+
+    public static double FTToCM(double ft) {
+        return ft * 30.48; // 1 ft = 30.48 cm
+    }
+}
