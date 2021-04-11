53940487df4473e5f2d2b12789138eb230d73683
Huseyin Salman <huseyinsalmanca@gmail.com>
2021-04-10 22:00:50 -0500

    Fix logic problems with DailyCaloricNeeds

--- app/src/main/java/com/group12/fitnics/business/DailyCaloricNeeds.java
+++ app/src/main/java/com/group12/fitnics/business/DailyCaloricNeeds.java
@@ -9,9 +9,21 @@ public class DailyCaloricNeeds {
         double dailyCaloricNeeds = 0;
 
         if (Character.compare(user.getGender(), 'M') == 0) {
-            bmr = 66 + (6.3 * user.getWeight()) + (12.9 * user.getHeight()) - (6.8 * user.getAge());
+            if (user.getUnits()[0] == 0) {
+                //BMR = 66.47 + ( 6.24 × weight in pounds ) + ( 12.7 × height in inches ) − ( 6.755 × age in years )
+                bmr = 66.47 + (6.24 * user.getWeight()) + (12.7 * user.getHeight()) - (6.755 * user.getAge());
+            } else if (user.getUnits()[0] == 1) {
+                //BMR = 66.47 + ( 13.75 × weight in kg ) + ( 5.003 × height in cm ) − ( 6.755 × age in years )
+                bmr = 66.47 + (13.75 * user.getWeight()) + (5.003 * user.getHeight()) - (6.755 * user.getAge());
+            }
         } else if (Character.compare(user.getGender(), 'F') == 0) {
-            bmr = 655 + (4.3 * user.getWeight()) + (4.7 * user.getHeight()) - (4.7 * user.getAge());
+            if (user.getUnits()[0] == 0) {
+                //BMR = 655.1 + ( 4.35 × weight in pounds ) + ( 4.7 × height in inches ) − ( 4.7 × age in years )
+                bmr = 655.1 + (4.35 * user.getWeight()) + (4.7 * user.getHeight()) - (4.7 * user.getAge());
+            } else if (user.getUnits()[0] == 1) {
+                //BMR = 655.1 + ( 9.563 × weight in kg ) + ( 1.85 × height in cm ) − ( 4.676 × age in years )
+                bmr = 655 + (9.563 * user.getWeight()) + (1.85 * user.getHeight()) - (4.676 * user.getAge());
+            }
         }
 
         if(user.getActivityLevel() == 0) {
